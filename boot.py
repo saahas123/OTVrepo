b699fa7ac9685b3850a5bf89df3eb7e1cd0bcc78
oGgame <70044124+saahas123@users.noreply.github.com>
2025-12-09 12:42:31 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -43,37 +43,36 @@ def navigateStage3():
     else:
         moveEnd()
 def moveUntilObstacle():
-    while(Sensors.getUltra1 > 0.1 or Sensors.getUltra2>0.1):
-        moveforward(40)
-
+    while(Sensors.getUltra1() > 15 or Sensors.getUltra2()>15):
+        move_forward(40)
     stop_all()
 def move_until_row2UP():
     while(enes100.y != 1.12):
-        moveforward(30)
+        move_forward(30)
     stop_all()
 def move_until_row2DOWN():
     while(enes100.y != 0.82):
-        moveforward(30)
+        move_forward(30)
     stop_all()
 def move_until_row3():
     while(enes100.y != 5):
-        moveforward(30)
+        move_forward(30)
 
     stop_all()
 
 def checkRowStart():
-     if(enes100.y > .9):
-        return 1;
+    if(enes100.y > .9):
+        return 1
     else:
-        return 3;
+        return 3
     
 def checkRow():
     if(enes100.y > 1.52):
-        return 1;
+        return 1
     elif(enes100.y < 1.52 and enes100.y > 0.42):
-        return 2;
+        return 2
     else:
-        return 3;
+        return 3
     
 
 def move_until_row2():
@@ -122,10 +121,8 @@ def turnLeftTo(targetTheta):
 
     while abs(normalize_angle(currentTheta - targetTheta)) > tolerance:
         time.sleep(0.05)
-        enes100.print("turning")
         currentTheta = normalize_angle(enes100.theta)
     
-    enes100.print("FIN")
 
     stop_all()
     time.sleep(0.05)
@@ -144,73 +141,84 @@ def navigateStage2():
         moveUntilObstacle()
 
         
+        enes100.print("starting nav")
 
         if row == 1:
-            turnRightTo(270)
+            turnRightTo(90)
             move_until_row2UP()
             turnLeftTo(0)
             prevRow = 1
+            enes100.print("turn to 2")
             row = 2
         elif row == 2:
             if prevRow == 1:
-                turnRightTo(270)
+                turnRightTo(90)
                 move_until_row3()
                 turnLeftTo(0)
                 row = 3
+                enes100.print("starting turn")
+
 
             elif prevRow == 3:
-                turnLeftTo(90)
+                turnLeftTo(270)
                 move_until_row1()
                 turnRightTo(0)
                 row = 1
+                enes100.print(" turn to 1")
+
 
             else:
-                turnLeftTo(90)
+                turnLeftTo(270)
                 move_until_row1()
                 turnRightTo(0)
                 row = 1
+                enes100.print("starting turn 1")
+
 
         else:  # row == 3
-            turnLeftTo(90)
+            turnLeftTo(270)
             move_until_row2DOWN()
             turnRightTo(0)
             prevRow = 3
             row = 2
+            enes100.print("starting 2")
+
 
 
     # -----------------------------
     # PART 2: Move until x < a
     # -----------------------------
     prevRow = None
+    enes100.print("part 2")
 
     while enes100.x < a:
         moveUntilObstacle()
 
 
         if row == 1:
-            turnRightTo(270)
+            turnRightTo(90)
             move_until_row2UP()
             turnLeftTo(0)
             prevRow = 1
 
         elif row == 2:
             if prevRow == 1:
-                turnRightTo(270)
+                turnRightTo(90)
                 move_until_row3()
                 turnLeftTo(0)
 
             elif prevRow == 3:
-                turnLeftTo(90)
+                turnLeftTo(270)
                 move_until_row1()
                 turnRightTo(0)
 
             else:
-                turnLeftTo(90)
+                turnLeftTo(270)
                 move_until_row1()
                 turnRightTo(0)
 
         else:  # row == 3
-            turnLeftTo(90)
+            turnLeftTo(270)
             move_until_row2DOWN()
             turnRightTo(0)
             prevRow = 3
@@ -218,9 +226,4 @@ def navigateStage2():
         
     
 enes100.print("starting turn")
-
-
-
-
-
-
+navigateStage2()
